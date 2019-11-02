cc65b120e04f8d654977710750e70907d8265fb4
fernando <fer@madforwebs.com>
2019-11-02 17:58:50 +0100

    cambios

--- Entity/Account.php
+++ Entity/Account.php
@@ -18,13 +18,6 @@ class Account
      */
     private $id;
 
-
-    /**
-     * @ORM\ManyToOne(targetEntity="CoreBundle\Entity\Year", inversedBy="accounts", cascade={"persist"})
-     * @ORM\JoinColumn(name="year", referencedColumnName="id", nullable=true)
-     */
-    private $year;
-
     /**
      * @var string
      *
@@ -38,30 +31,18 @@ class Account
      * @ORM\Column(name="balance", type="float",precision=2)
      */
     private $balance;
+    
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
 
     /**
-     * @ORM\OneToMany(targetEntity="Expenditure", mappedBy="account", cascade={"all"})
-     * @ORM\OrderBy({"dateBuy"="DESC"})
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime")
      */
-    private $expenditures;
-
-    /**
-     * @ORM\OneToMany(targetEntity="Income", mappedBy="account", cascade={"all"})
-     * @ORM\OrderBy({"dateIncome"="DESC"})
-     */
-    private $earnings;
-
-    /**
-     * @ORM\OneToMany(targetEntity="Income", mappedBy="accountDestiny", cascade={"all"})
-     * @ORM\OrderBy({"dateIncome"="DESC"})
-     */
-    private $earningsOrigins;
-
-    /**
-     * @ORM\OneToMany(targetEntity="Bill", mappedBy="account", cascade={"all"})
-     * @ORM\OrderBy({"client"="ASC"})
-     */
-    private $bills;
+    private $updatedAt;
 
 
     /**
@@ -75,43 +56,6 @@ class Account
     }
 
 
-    public function totalEarnings()
-    {
-        $sum = 0;
-
-        if (count($this->getEarnings())) {
-            /** @var Income $earning */
-            foreach ($this->getEarnings() as $earning) {
-                $sum += $earning->getTotal();
-            }
-        }
-
-        return $sum;
-    }
-
-    public function totalExpenditures()
-    {
-        $sum = 0;
-        if (count($this->getExpenditures())) {
-            /** @var Expenditure $earning */
-            foreach ($this->getExpenditures() as $earning) if ($earning->getStatus() == 'paid') {
-                $sum += $earning->getTotal();
-            }
-        }
-        return $sum;
-    }
-
-    public function totalBills()
-    {
-        $sum = 0;
-        if (count($this->getBills())) {
-            /** @var Bill $earning */
-            foreach ($this->getBills() as $earning) if ($earning->getPaid()) {
-                $sum += $earning->calculateTotal();
-            }
-        }
-        return $sum;
-    }
 
     /**
      * @ORM\PrePersist()
@@ -175,129 +119,5 @@ class Account
     {
         return ($this->getName() == '') ? '' : $this->getName();
     }
-
-    /**
-     * @return mixed
-     */
-    public function getExpenditures()
-    {
-        return $this->expenditures;
-    }
-
-    /**
-     * @param mixed $expenditures
-     */
-    public function setExpenditures($expenditures)
-    {
-        $this->expenditures = $expenditures;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEarnings()
-    {
-        return $this->earnings;
-    }
-
-    /**
-     * @param mixed $earnings
-     */
-    public function setEarnings($earnings)
-    {
-        $this->earnings = $earnings;
-    }
-
-    public function addEarning($earning)
-    {
-        $this->earnings[] = $earning;
-    }
-
-    public function removeEarning($earning)
-    {
-        $this->earnings->removeElement($earning);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getBills()
-    {
-        return $this->bills;
-    }
-
-    /**
-     * @param mixed $bills
-     */
-    public function setBills($bills)
-    {
-        $this->bills = $bills;
-    }
-
-    public function addExpenditure($expenditure)
-    {
-        $this->expenditures[] = $expenditure;
-    }
-
-    public function removeExpenditure($expenditure)
-    {
-        $this->expenditures->removeElement($expenditure);
-    }
-
-
-    public function addBill($bill)
-    {
-        $this->bills[] = $bill;
-    }
-
-    public function removeBill($bill)
-    {
-        $this->bills->removeElement($bill);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEarningsOrigins()
-    {
-        return $this->earningsOrigins;
-    }
-
-    /**
-     * @param mixed $earningsOrigins
-     */
-    public function setEarningsOrigins($earningsOrigins)
-    {
-        $this->earningsOrigins = $earningsOrigins;
-    }
-
-    public function addEarningOrigin($earningOrigin)
-    {
-        $this->earningsOrigins[] = $earningOrigin;
-    }
-
-    public function removeEarningOrigin($earningOrigin)
-    {
-        $this->earningsOrigins->removeElement($earningOrigin);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getYear()
-    {
-        return $this->year;
-    }
-
-    /**
-     * @param mixed $year
-     */
-    public function setYear($year)
-    {
-        $this->year = $year;
-    }
-
-
-
 }
 
